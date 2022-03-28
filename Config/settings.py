8fd9eab1b12096b317a2454c940f9a3c2c3ba3be
NeiroFucker <60180871+NeiroFucker@users.noreply.github.com>
2022-03-28 20:49:21 +0500

    Update settings.py

--- Config/settings.py
+++ Config/settings.py
@@ -11,10 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from boto.s3.connection import S3Connection
-
-
-s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
